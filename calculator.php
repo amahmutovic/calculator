66257e4b48257fb9c68e6d4d52d3d190b14ccd38
amahmutovic <amahmutovic@capeannenterprises.com>
2015-01-30 14:53:12 +0100

    add function in declartion! ARMIN shame on you

--- calculator.php
+++ calculator.php
@@ -2,11 +2,11 @@
 class Calculator
 {
 	
-	public sub($par1,$par2)
+	public function sub($par1,$par2)
 	{
 		return $par1-$par2;
 	}
-	public add($par1,$par2)
+	public function add($par1,$par2)
 	{
 		return $par1+$par2;
 	}
